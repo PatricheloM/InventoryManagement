81d89de4ce25f6a94c00962112517124b33ba7ff
Kun Patrik <kunpatrik00@mailbox.unideb.hu>
2022-11-19 15:15:10 +0100

    removed unnecessary import

--- backend/src/main/java/inventorymanagement/backend/controller/ItemController.java
+++ backend/src/main/java/inventorymanagement/backend/controller/ItemController.java
@@ -25,7 +25,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Optional;
 
